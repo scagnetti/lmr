e551b2bd2cd3715c4f8f1242e3a4c3503fcead99
sca <sca@deepthought.(none)>
2013-02-20 22:45:41 +0100

    Adjusted capistrano version to supprt RVM on the server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,11 @@
 require 'bundler/capistrano'
+# Add RVM's lib directory to the load path.
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+# Load RVM's capistrano plugin.    
+require "rvm/capistrano"
+
+set :rvm_ruby_string, '1.9.3'
+set :rvm_type, :user  # Don't use system-wide RVM
 
 # deploy config
 set :application, "lmr"
