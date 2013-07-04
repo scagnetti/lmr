32330484301c4c09e792bd8226f1e72cd2f444aa
sca <sca@deepthought.(none)>
2013-07-04 20:34:53 +0200

    Added exception for changes in HTML structure

---
+++ lib/engine/exceptions/unexpected_html_structure.rb
@@ -0,0 +1,5 @@
+# This exception signals that a HTML element could not be accessed as expected.
+# Further execution of the programm logic would inevitable fail. 
+class UnexpectedHtmlStructure < RuntimeError
+  
+end
