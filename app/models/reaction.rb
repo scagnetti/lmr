3621bbaf990f6ad07d79b0ea6d91f00d94582a19
Volker <volker.kindermann@gmail.com>
2014-06-14 15:34:49 +0200

    Updated to Rails 3.2.17

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,4 +1,7 @@
 class Reaction < ActiveRecord::Base
   attr_accessible :index_after, :index_before, :price_after, :price_before, :release_date, :before, :after
   has_many :score_cards
+  
+  validates :index_after, :index_before, :price_after, :price_before, :share_perf, :index_perf, presence: true
+  validates :index_after, :index_before, :price_after, :price_before, :share_perf, :index_perf, numericality: true
 end
