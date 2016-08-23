a2ec4342dfdb96452f542784b03b0b08a885091b
scagnetti <scagnetti@gmx.net>
2016-08-23 22:36:08 +0200

    Fixed querey date in rising prices controller

--- app/controllers/rising_scores_controller.rb
+++ app/controllers/rising_scores_controller.rb
@@ -2,7 +2,7 @@ class RisingScoresController < ApplicationController
   # GET /rising_scores
   # GET /rising_scores.json
   def index
-    @rising_scores = RisingScore.joins('INNER JOIN shares ON shares.isin = rising_scores.isin').where("rising_scores.created_at >= ?", Time.zone.now.beginning_of_day).order("rising_scores.days DESC").page(params[:page]).per(20)
+    @rising_scores = RisingScore.joins('INNER JOIN shares ON shares.isin = rising_scores.isin').where("rising_scores.created_at >= ?", (Time.zone.now - 1.day).beginning_of_day).order("rising_scores.days DESC").page(params[:page]).per(20)
 
     respond_to do |format|
       format.html # index.html.erb
